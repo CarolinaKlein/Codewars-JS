72dbb5b1753f26c2496e5013892e748a8e8dbe77
Carolina Klein <carolina.e.klein@gmail.com>
2020-05-17 10:23:27 -0400

    more solutions added to casingStrings.js

--- 8_kyu/casingStrings.js
+++ 8_kyu/casingStrings.js
@@ -1,3 +1,5 @@
+//ex: capitalize the first letter of each word
+
 
 //My solution
 
@@ -11,10 +13,29 @@ function toJadenCase(string){
   console.log(str);
   
 
-  //How it should have been written to pass Code Wars Check (basically, with the this. syntax)
+  //How it should have been written to pass Code Wars Check (basically, with the "this." syntax)
 
   String.prototype.toJadenCase = function () { 
     return this.split(" ").map(function(word){
       return word.charAt(0).toUpperCase() + word.slice(1);
     }).join(" ");
   }
+
+
+  //Solution that is hard to understand, but best practice:
+
+  String.prototype.toJadenCase = function () {
+    return this.replace(/(^|\s)[a-z]/g, function(x){ return x.toUpperCase(); });
+  };
+
+  //using a loop 
+
+  String.prototype.toJadenCase = function () {
+    var words = this.split(' ');
+  
+    for(var i = 0, wordsLen = words.length; i < wordsLen; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+  
+    return words.join(' ');
+  };
