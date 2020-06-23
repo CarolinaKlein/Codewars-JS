8a4b85acf054f941f162710976a9551982d55c81
Carolina Klein <carolina.e.klein@gmail.com>
2020-06-23 17:37:10 -0400

    added solutions created by other users

--- 8_kyu/sumOfPositive.js
+++ 8_kyu/sumOfPositive.js
@@ -8,3 +8,27 @@ function positiveSum(arr) {
 	}
 	return count;
 }
+
+// Solution with Reduce method
+
+function positiveSum(arr){
+    return arr.reduce((a,b) => a + (b > 0 ? b : 0, 0))
+}
+  //or
+const positiveSum = (arr) => arr.reduce((sum, n) => n > 0 ? sum + n : sum, 0);
+
+// Solution with the Reduce and filter method
+
+function positiveSum(arr){
+    return arr.filter(x => x >= 0).reduce((a,b) => a + b, 0)
+}
+
+// Solution that I don't understant why they used the i and the a for 
+
+function positiveSum(arr) {
+    var sum = 0;
+    arr.forEach(function(v,i,a) {
+      if(v>0){sum+=v;}
+    });
+    return sum;
+  }
