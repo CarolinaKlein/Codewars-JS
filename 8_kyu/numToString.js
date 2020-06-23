f8e39ff767e411096ebd205d6d59eb111896b9e5
Carolina Klein <carolina.e.klein@gmail.com>
2020-06-23 11:04:19 -0400

    added other interesting solutions from other codewars users

--- 8_kyu/numToString.js
+++ 8_kyu/numToString.js
@@ -6,3 +6,22 @@ function numberToString(num) {
 
 console.log(numberToString(123))
 
+//Solution 2
+
+function numberToString(num) {
+    return String(num);
+  }
+
+//Solution 3
+
+const numberToString = num => `${num}`
+
+//Solution 4
+
+const numberToString = num => num.toString()
+
+//Solution 5
+
+function numberToString(num) {
+    return ''+num;
+  }
