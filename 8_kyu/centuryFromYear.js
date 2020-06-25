422b3066194fdef321251427cc21b9e67c4fbc7c
Carolina Klein <carolina.e.klein@gmail.com>
2020-06-25 17:51:10 -0400

    added other solutions

--- 8_kyu/centuryFromYear.js
+++ 8_kyu/centuryFromYear.js
@@ -12,10 +12,34 @@ function century(year){
 function century(year) {
     return Math.ceil(year/100); //using ceiling method to round up to nearest century (100)
   }
-//Solution 2
+//Most Simple 
 
 function century(year) {
     return (year + 99) / 100 | 0;
 }
 
+//ParseInt solution
 
+function century(year) {
+    if (year <= 100){
+      return 1;
+    }
+  
+    let cen = parseInt(year / 100);
+    let rem = year % 100;
+    
+    return rem === 0 ? cen : cen + 1;
+}
+
+// Loop Solution
+
+function century(year) {
+    var century = 0;
+  
+    for(var i = 0; i < year; i++) {
+      if(i % 100 == 0) {
+        century++;
+      }
+    }
+    return century;
+  }
